6bf87d66fc139cceadbf5ed153ce08ec1b0e4e17
Lorenzo Davidson <lorenzodavidson@Lorenzos-MacBook-Pro.local>
2023-06-05 11:18:20 -0700

    First commit

---
+++ r4ds_ch6.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(nycflights13)
+
+not_cancelled <- flights %>% 
+  filter(!is.na(dep_delay)█, !is.na(arr_delay))
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(mean = mean(dep_delay))
